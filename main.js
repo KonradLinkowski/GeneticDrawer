265f1daeb91b1d216538b1dff87fa306261d148a
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-10-27 18:30:12 +0200

    Added alpha

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ function drawShape(ctx, color, coords) {
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
-  for (var i = 0; i < 6; i++) {
+  for (var i = 0; i < 8; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
