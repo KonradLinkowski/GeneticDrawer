5c9732bf266790b12e3f458ef402a883fd5cfdac
Asfo <asfoone@gmail.com>
2020-10-07 18:22:33 -0500

    Image swap with drag and drop

---
+++ assets/js/main.js
@@ -0,0 +1,242 @@
+const canvas = document.querySelector("#genetic-canvas");
+let image = document.getElementById("genetic-image");
+const matchSpan = document.querySelector("#match_percentage");
+const genSpan = document.querySelector("#gen_count");
+
+const ctx = canvas.getContext("2d");
+const helpCanvas = document.createElement("canvas");
+const helpContext = helpCanvas.getContext("2d");
+
+resizeCanvas(image.width, image.height);
+let mainImageData = getImageData(image);
+
+let iteration = null;
+let drawBestIteration = null;
+
+genetic();
+
+let dropArea = document.getElementById("dropzone");
+
+["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, preventDefaults, false);
+  document.body.addEventListener(eventName, preventDefaults, false);
+});
+["dragenter", "dragover"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, highlight, false);
+});
+["dragleave", "drop"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, unhighlight, false);
+});
+
+dropArea.addEventListener("drop", handleDrop, false);
+
+function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+function highlight(e) {
+  dropArea.classList.add("highlight");
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove("highlight");
+}
+
+function handleDrop(e) {
+  const dt = e.dataTransfer;
+  const file = dt.files;
+
+  handleFiles(file);
+}
+
+function handleFiles(file) {
+  if (file.length > 1) {
+    alert("Only 1 image can be used");
+  } else {
+    let reader = new FileReader();
+	reader.readAsDataURL(file[0]);
+	reader.onloadend = function () {
+		image.src = reader.result;
+		image.onload = function() {
+			resizeCanvas(image.width, image.height);
+			mainImageData = getImageData(image);
+			genetic();
+		}
+	};
+  }
+}
+
+function genetic() {
+  const maxDiff = mainImageData.width * mainImageData.height * 3 * 255;
+  const popSize = 50;
+  const olds = new Array(popSize);
+  const news = new Array(popSize);
+  let theBest = olds[0];
+  let generation = 0;
+  
+  if(drawBestIteration !== null) {
+	clearInterval(drawBestIteration);
+	drawBestIteration = null;
+  }
+  
+  const defaultFit = calcFitness(
+    mainImageData.data,
+    new ImageData(image.width, image.height).data
+  );
+  
+  for (let i = 0; i < olds.length; i++) {
+    olds[i] = {
+      imageData: new ImageData(image.width, image.height),
+      fitness: defaultFit
+    };
+    news[i] = {
+      imageData: new ImageData(image.width, image.height),
+      fitness: defaultFit
+    };
+  }
+  
+  iteration();
+  drawBestIteration = setInterval(drawBest, 100);
+  
+  function drawBest() {
+    genSpan.textContent = generation;
+    matchSpan.textContent = (100 * (1 - theBest.fitness / maxDiff)).toFixed(2);
+    ctx.putImageData(theBest.imageData, 0, 0);
+  }
+  
+  function iteration() {
+    generation += 1;
+    for (let i = 0; i < olds.length; i++) {
+      helpContext.putImageData(olds[i].imageData, 0, 0);
+      const color = getRandomColor();
+      const coords = [
+        getRandomInt(0, image.width),
+        getRandomInt(0, image.height),
+        getRandomInt(0, image.width),
+        getRandomInt(0, image.height),
+        getRandomInt(0, image.width),
+        getRandomInt(0, image.height)
+      ];
+      if (Math.random() < 0.5) {
+        drawCircle(
+          helpContext,
+          color,
+          getRandomInt(0, image.width),
+          getRandomInt(0, image.height),
+          getRandomFloat(1, Math.min(image.width / 2, image.height / 2))
+        );
+      } else {
+        drawShape(helpContext, color, coords);
+      }
+      const imgData = helpContext.getImageData(0, 0, image.width, image.height);
+      news[i].imageData = imgData;
+      news[i].fitness = calcFitness(mainImageData.data, imgData.data);
+    }
+    const bests = [...findXBest(olds, 5), ...findXBest(news, 5)];
+    bests.sort(sortFitness);
+    theBest = bests[0];
+    for (let i = 10; i < olds.length; i++) {
+      olds[i].imageData = bests[i % 10].imageData;
+      olds[i].fitness = bests[i % 10].fitness;
+    }
+    setTimeout(iteration, 0);
+  }
+}
+
+function sortFitness(a, b) {
+  return a.fitness - b.fitness;
+}
+
+function findXBest(arr, x) {
+  if (arr.length < x) throw new Error("Array length must be greater than x.");
+  const bests = [];
+  const indexes = [];
+  for (let c = 0; c < x; c++) {
+    let min = { fitness: Number.MAX_SAFE_INTEGER };
+    let minIndex = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (indexes.includes(i)) continue;
+      if (arr[i].fitness < min.fitness) {
+        min = arr[i];
+        minIndex = i;
+      }
+    }
+    bests.push(min);
+    indexes.push(minIndex);
+  }
+  return bests;
+}
+
+function resizeCanvas(width, height) {
+	canvas.width = helpCanvas.width = width;
+	canvas.height = helpCanvas.height = height;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function getImageData(image) {
+  helpCanvas.width = image.width;
+  helpCanvas.height = image.height;
+  helpContext.drawImage(image, 0, 0);
+  const data = helpContext.getImageData(0, 0, image.width, image.height);
+  helpContext.clearRect(0, 0, helpCanvas.width, helpCanvas.height);
+  return data;
+}
+
+function mutate(m, t) {
+  const n = [];
+  for (let i = 0; i < m.length; i++) {
+    n.push(Math.random() < 0.5 ? m[i] : t[i]);
+  }
+  return n;
+}
+
+function calcFitness(original, current) {
+  let sum = 0;
+  for (let i = 0; i < original.length; i += 4) {
+    sum +=
+      Math.abs(original[i] - current[i]) + // red
+      Math.abs(original[i + 1] - current[i + 1]) + // green
+      Math.abs(original[i + 2] - current[i + 2]); // blue
+  }
+  return sum;
+}
+
+function drawCircle(ctx, color, x, y, radius) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawShape(ctx, color, coords) {
+  // the triangle
+  ctx.beginPath();
+  ctx.moveTo(coords[0], coords[1]);
+  for (let i = 2; i < coords.length; i += 2) {
+    ctx.lineTo(coords[i], coords[i + 1]);
+  }
+  ctx.closePath();
+
+  // the fill color
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 8; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
